3f8e7a282c1742b077fd188060cf51dd60514c2e
Nicholas Paun <nicholas@transit.app>
2022-09-16 13:13:07 -0700

    Add schema

---
+++ bus_believability/schema.sql
@@ -0,0 +1,1 @@
+CREATE TABLE vehicle_updates (start_date int, trip_id text, route_id text, direction_id int, lat real, lon real, speed real, stop_sequence int, stop_id text, vehicle_id text, vehicle_status int, observed_at int, unique(start_date, trip_id, stop_sequence));
